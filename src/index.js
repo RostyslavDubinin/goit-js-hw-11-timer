33c0e0d354b95b0622a3609b5da0c20b2f5af19a
RostyslavDubinin <75220575+RostyslavDubinin@users.noreply.github.com>
2021-05-23 12:57:09 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-const targetDate = new Date("jun 17, 2021 12:00:00").getTime();
-const timer = setInterval(function() {
-const now = new Date().getTime();
-const time = targetDate - now;
+
+class CountdownTimer {
+    constructor({ onTick }) {
+        this.onTick = onTick;
+    }
+    start() { 
+        const startTime = new Date("jun 17, 2021 12:00:00");
+        setInterval (() => {
+            const currentTime = Date.now();
+            const deltaTime = startTime - currentTime;
+            const timeUpdate = this.getTimeComponents(deltaTime);
+            
+
+            
+            this.onTick(timeUpdate);
+        }, 1000);
+    }
+
+
+    pad(value) {
+        return String(value).padStart(2, '0');
+    }
     
-    const days = Math.floor(time / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    getTimeComponents(time) {
+        const days = Math.floor(time / (1000 * 60 * 60 * 24));
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+          
+        return {days, hours, mins, secs };
+    } 
+
     
-    document.querySelector('[data-value="days"]').innerHTML = days;
-    document.querySelector('[data-value="hours"]').innerHTML = ("0"+hours).slice(-2);
-    document.querySelector('[data-value="mins"]').innerHTML = ("0"+mins).slice(-2);
-    document.querySelector('[data-value="secs"]').innerHTML = ("0"+secs).slice(-2);
-    
-    
-}, 1000);
+
+}
+
+const timer = new CountdownTimer({
+    onTick: updateClockFace,
+  });
+
+  timer.start();
+
+function updateClockFace({ days, hours, mins, secs }) {
+    document.querySelector('[data-value="days"]').innerHTML = `${days}`;
+    document.querySelector('[data-value="hours"]').innerHTML = `${hours}`;
+    document.querySelector('[data-value="mins"]').innerHTML = `${mins}`;
+    document.querySelector('[data-value="secs"]').innerHTML = `${secs}`;
+}; 
+
