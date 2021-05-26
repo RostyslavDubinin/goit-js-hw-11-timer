c1e4a3608d3b14362468f5a27be9d9086464ddba
RostyslavDubinin <75220575+RostyslavDubinin@users.noreply.github.com>
2021-05-26 22:47:21 +0300

    2

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 
 class CountdownTimer {
-    constructor({ onTick }) {
+    constructor({ selector,  targetDate, onTick}) {
+        this.selector = selector;
+        this.targetDate = targetDate;
         this.onTick = onTick;
+
     }
     start() { 
-        const startTime = new Date("jun 17, 2021 12:00:00");
+        const targetDate = new Date("jun 17, 2021 12:00:00");
         setInterval (() => {
             const currentTime = Date.now();
-            const deltaTime = startTime - currentTime;
+            const deltaTime = targetDate - currentTime;
             const timeUpdate = this.getTimeComponents(deltaTime);
             
 
@@ -35,15 +38,23 @@ class CountdownTimer {
 }
 
 const timer = new CountdownTimer({
-    onTick: updateClockFace,
+    selector: '#timer-1',
+    targetDate: new Date('Jul 17, 2019'),
+    onTick: updateClockFace
   });
 
   timer.start();
 
+  const daysSpan = document.querySelector('[data-value="days"]');
+  const hoursSpan = document.querySelector('[data-value="hours"]');
+  const minutesSpan = document.querySelector('[data-value="mins"]');
+  const secondsSpan = document.querySelector('[data-value="secs"]');
+
+
 function updateClockFace({ days, hours, mins, secs }) {
-    document.querySelector('[data-value="days"]').innerHTML = `${days}`;
-    document.querySelector('[data-value="hours"]').innerHTML = `${hours}`;
-    document.querySelector('[data-value="mins"]').innerHTML = `${mins}`;
-    document.querySelector('[data-value="secs"]').innerHTML = `${secs}`;
+    daysSpan.textContent = `${days}`;
+    hoursSpan.textContent = `${hours}`;
+    minutesSpan.textContent = `${mins}`;
+    secondsSpan.textContent = `${secs}`;
 }; 
 
